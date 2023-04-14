c02ce1d6d43da581754fcde34fc43323b7a73ead
Harshal Sheth <hsheth2@gmail.com>
2023-04-13 19:01:58 -0700

    update script to print profile

--- impact_analysis.sh
+++ impact_analysis.sh
@@ -13,21 +13,24 @@ pip install acryl-datahub==0.10.1.2rc8
 pip cache remove 'acryl*'
 
 # Replace the DBT_PROFILE_NAME with the actual profile name.
-sed -i "s/DBT_PROFILE_NAME/${DBT_PROFILE_NAME}/g" ${DBT_PROFILES_DIR}/profiles.yml
+sed -i "s/DBT_PROFILE_NAME/${DBT_PROFILE_NAME}/g" "${DBT_PROFILES_DIR}/profiles.yml"
+cat "${DBT_PROFILES_DIR}/profiles.yml"
 
 # Generate the previous manifest.
-git checkout ${GITHUB_BASE_REF}
+git checkout "${GITHUB_BASE_REF}"
 dbt ls
 cp -r target target-previous
 git checkout -
 
 # Run impact analysis script.
-DBT_ARTIFACT_STATE_PATH=target-previous python ${GITHUB_ACTION_PATH}/impact_analysis.py
+DBT_ARTIFACT_STATE_PATH=target-previous python "${GITHUB_ACTION_PATH}/impact_analysis.py"
 cat impact_analysis.md
 
 # Output a multiline string to an output parameter.
 # Technique from https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#multiline-strings
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "IMPACT_ANALYSIS_MD<<$EOF" >> $GITHUB_OUTPUT
-cat impact_analysis.md >> $GITHUB_OUTPUT
-echo "$EOF" >> $GITHUB_OUTPUT
+{
+	echo "IMPACT_ANALYSIS_MD<<$EOF"
+	cat impact_analysis.md
+	echo "$EOF"
+} >> $GITHUB_ENV
